8a57bfde0a207d29badb1816099d75b99c051b06
slambrisca <santiagolambrisca@gmail.com>
2014-05-26 13:09:47 -0300

    cambios

--- src/repetidor.py
+++ src/repetidor.py
@@ -8,7 +8,7 @@ scheduler = sched.scheduler(time.time, time.sleep)
 
 now = time.time()
 
-for i in range(0, 12):
+for i in range(0, 10):
 	print "encolo para dentro de " + str(i) + " horas"
 	temp = now+3600*i								#3600 representa una hora
 	scheduler.enterabs(temp, 1, main, (4,0.5,True))
--- src/trace.py
+++ src/trace.py
@@ -8,8 +8,10 @@ from mytraceroute import *
 def main(pkt, umbral, localizacion):
     hosts = {
         "www.ubc.ca"       : "canada_%s.txt",
-        #"www.msu.ru"       : "rusia_%s.txt",
-        #"www.cuhk.edu.hk"  : "china_%s.txt"
+        "www.msu.ru"       : "rusia_%s.txt",
+        "www.cuhk.edu.hk"  : "china_%s.txt",
+	"www.nus.edu.ws"   : "samoa_%s.txt",
+	"web.up.ac.za"     : "safrica_%s.txt"
     }
 
     #hosts = { "www.google.com" : "google.txt"}
