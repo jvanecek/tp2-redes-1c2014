ebfff639269bfb013beb8b52ab45784a62185752
juan2511 <juann.vanecek@gmail.com>
2014-05-27 03:55:43 -0300

    Graficador y algunos graficos

--- src/repetidor.py
+++ src/repetidor.py
@@ -8,7 +8,7 @@ scheduler = sched.scheduler(time.time, time.sleep)
 
 now = time.time()
 
-for i in range(0, 1):
+for i in range(0, 9):
 	print "encolo para dentro de " + str(i) + " horas"
 	temp = now+3600*i								#3600 representa una hora
 	scheduler.enterabs(temp, 1, main, (4,0.5,True,2))
